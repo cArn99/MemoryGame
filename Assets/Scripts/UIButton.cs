eaf3eadf85e234855b653b1332950ecaf84e435c
cArn99 <ahadk630@hotmail.com>
2021-10-27 17:23:37 +0530

    Initial commit

---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour
+{
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private string targetMessage;
+    public Color highlightColor = Color.yellow;
+
+    public void OnMouseEnter()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if(sprite!=null)
+        {
+            sprite.color = highlightColor;
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if(sprite!=null)
+        {
+            sprite.color = Color.white;
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+    }
+
+    public void OnMouseUp()
+    {
+        transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+        if (targetObject != null)
+        {
+            targetObject.SendMessage(targetMessage);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
